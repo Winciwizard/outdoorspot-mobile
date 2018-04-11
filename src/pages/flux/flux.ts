a464133cc8e5f2808681811475219bfa22b87e62
Winciwizard <vincent.verges@gmail.com>
2018-04-11 14:46:30 +0200

    Lien de flux à spot fait

--- src/pages/flux/flux.ts
+++ src/pages/flux/flux.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {PostApiProvider} from "../../providers/post-api/post-api";
+import {MovieDetailPage} from "../../../../TUTO1/movie-app/src/pages/movie-detail/movie-detail";
+import {SpotPage} from "../spot/spot";
 
 /**
  * Generated class for the FluxPage page.
@@ -29,6 +31,10 @@ export class FluxPage {
     this.postApiProvider.getPosts().subscribe(data => {
       this.posts = data;
     });
+  }
+
+  goToDetail(post: any) {
+    this.navCtrl.push(SpotPage, post);
   }
 
 }
