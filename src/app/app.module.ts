4699bdf35ad073b28b4c28f10a544a9e9ecd6cb2
Winciwizard <vincent.verges@gmail.com>
2018-04-11 10:57:39 +0200

    Ajout page flux dans app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,13 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {FluxPage} from "../pages/flux/flux";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    FluxPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    FluxPage
   ],
   providers: [
     StatusBar,
