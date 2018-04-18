b540fa6b5d03bf2274fc180292dfeb0c02a4aedd
sabso7 <t.bassobondini@gmail.com>
2018-04-18 14:21:20 +0200

    affichage des contacts

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {FriendApiProvider} from "../../providers/friend-api/friend-api";
 import {Storage} from "@ionic/storage";
 import {UserApiProvider} from "../../providers/user-api/user-api";
+import {UserProfilPage} from "../user-profil/user-profil";
 
 @Component({
   selector: 'page-contact',
@@ -12,6 +13,8 @@ export class ContactPage {
 
   friends: any[];
   currentuser: any[];
+  myFriends: any [];
+  myFriendsList = [];
 
 
   constructor(public navCtrl: NavController,
@@ -20,31 +23,43 @@ export class ContactPage {
               private storage: Storage,
               private userApiProvider: UserApiProvider) {
     this.initializeFriend();
-
-
   }
 
 
   initializeFriend() {
 
-    this.friendApiProvider.getfriend().subscribe(data => {
-      this.friends = data[0];
-    })
+    this.storage.get('user_id').then((val) => {
+      this.currentuser = val;
+    }).then((va) => {
+      this.friendApiProvider.getfriend().subscribe(data => {
+        this.friends = data;
+        this.myFriends = this.friends.filter((value) => {
+          return (value.user_id === this.currentuser);
+        });
+        this.myFriends = this.myFriends[0]['friend'];
+
+        this.myFriends.forEach((id) => {
+
+          this.userApiProvider.getUser(id).subscribe((info) => {
+
+              this.myFriendsList.push(info['data'])
+
+            }
+          );
+
+        });
+      });
+    });
+    console.log(this.myFriendsList);
 
   }
 
-  // getCurrentUser() {
-  //   this.storage.get('user_id').then((val) => {
-  //     console.log(val);
-  //     this.userApiProvider.getUser(val).subscribe(data => {
-  //       console.log(data['data']);
-  //       this.user = data['data'];
-  //     })
-  //   })
-  // }
+  goToProfil(amis){
+
+    this.navCtrl.push(UserProfilPage, amis);
 
 
-
+  }
 
 
 
