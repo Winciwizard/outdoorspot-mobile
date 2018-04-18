72cfcfbb1d8258198dc3fd4942bea7766f2ee880
Winciwizard <vincent.verges@gmail.com>
2018-04-18 17:01:09 +0200

    bugfix

--- src/pages/share/share.ts
+++ src/pages/share/share.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Camera, CameraOptions} from "@ionic-native/camera";
 
 /**
  * Generated class for the SharePage page.
@@ -18,8 +17,7 @@ export class SharePage {
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams,
-    private camera: Camera) {
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
