dafefe974606d500e1174a2c39d065a027a7be2a
sandyann22 <sandycerdangras@orange.fr>
2018-04-18 11:57:28 +0200

    geolocalisation ajouter tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import {LoginPage} from "../login/login";
 import {ProfilPage} from "../profil/profil";
@@ -22,21 +21,71 @@ export class TabsPage {
   tab4Root = SearchPage;
   tab5Root = ProfilPage;
 
+  Lat: any;
+  Long: any;
 
   constructor(
     public navCtlr: NavController,
     public navParams: NavParams,
     private storage: Storage)
-  {}
+
+  {this.locate();}
+  locate() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.Lat = position.coords.latitude;
+      this.Long = position.coords.longitude;
+
+      console.log('hello');
+
+
+    }, err => console.log(err));
+    var onSuccess = function (position) {
+      console.log('Latitude: ' + position.coords.latitude + '\n' +
+        'Longitude: ' + position.coords.longitude + '\n' +
+        'Altitude: ' + position.coords.altitude + '\n' +
+        'Accuracy: ' + position.coords.accuracy + '\n' +
+        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+        'Heading: ' + position.coords.heading + '\n' +
+        'Speed: ' + position.coords.speed + '\n' +
+        'Timestamp: ' + position.timestamp + '\n');
+
+    };
+
+    function onError(error) {
+      alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+    }
+
+    var intervalID;
+
+    function geolocAgain() {
+      intervalID = setInterval(nav, 10000);
+    }
+
+    function nav() {
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+    }
+    geolocAgain();
+
+  }
 
   ionViewDidLoad() {
+
     console.log('ionViewDidLoad Tabs');
+    //stockage user_id
     this.storage.get('user_id').then((val) => {
       if(val == null){
         this.navCtlr.setRoot(LoginPage);
       }
     })
+
+    //Geolocalisation
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+      console.log("navigator.geolocation works well");
   }
 
-}
+}}
 
