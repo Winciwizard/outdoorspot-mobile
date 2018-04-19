0f977a9463c63e4a965c7274b17ed1b25a2db380
Winciwizard <vincent.verges@gmail.com>
2018-04-19 16:59:39 +0200

    MAJ page user-profil

--- src/pages/user-profil/user-profil.ts
+++ src/pages/user-profil/user-profil.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {UserApiProvider} from "../../providers/user-api/user-api";
 import {PostApiProvider} from "../../providers/post-api/post-api";
-import {Storage} from "@ionic/storage";
 import {SpotPage} from "../spot/spot";
 
 /**
@@ -19,34 +18,39 @@ import {SpotPage} from "../spot/spot";
 })
 export class UserProfilPage {
 
+  //Déclaration des variables
   user  = [];
   posts = [];
   myPost;
 
+  //Déclaration des classes de navigation et d'appels d'API
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private userApiProvider: UserApiProvider,
-
               private postApiProvider: PostApiProvider,
-              private storage: Storage
   ) {}
 
-  ionViewDidLoad() {
+  //Fonction de chargement, récupére l'id de l'utilisateur et ses information
+  ionViewDidLoad()
+  {
     console.log('ionViewDidLoad ProfilPage');
     this.user = this.navParams.data;
     console.log(this.user['id']);
 
-    this.postApiProvider.getPosts().subscribe(data => {
+    this.postApiProvider.getPosts().subscribe(data =>
+    {
         this.posts = data['data'];
         console.log(this.posts);
-        this.myPost = this.posts.filter((post) => {
+        this.myPost = this.posts.filter((post) =>
+        {
           return (post.user_id === this.user['id'])
         })
-
-      })
+    })
   }
 
-  goToDetail(post) {
+
+  goToDetail(post)
+  {
     this.navCtrl.push(SpotPage, post);
   }
 
