da5b0f2247000eba64504a67205d7122e8c4ea7e
Winciwizard <vincent.verges@gmail.com>
2018-04-19 16:22:13 +0200

    MAJ page profil

--- src/pages/profil/profil.ts
+++ src/pages/profil/profil.ts
@@ -4,7 +4,6 @@ import {UserApiProvider} from "../../providers/user-api/user-api";
 import {Storage} from "@ionic/storage";
 import {PostApiProvider} from "../../providers/post-api/post-api";
 import {SpotPage} from "../spot/spot";
-import {FriendApiProvider} from "../../providers/friend-api/friend-api";
 
 /**
  * Generated class for the ProfilPage page.
@@ -20,11 +19,13 @@ import {FriendApiProvider} from "../../providers/friend-api/friend-api";
 })
 export class ProfilPage {
 
+  //Déclaration des variables
   user  = [];
   posts = [];
   idUser;
   myPost;
 
+  //Déclaration des classes de navigation, d'appels d'API et de stockage local
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private userApiProvider: UserApiProvider,
@@ -33,35 +34,36 @@ export class ProfilPage {
               private storage: Storage
   ) {}
 
-  ionViewDidLoad() {
+  //Fonction de chargement, récupere les informations de l'utilisateur connceté et ses posts
+  ionViewDidLoad()
+  {
     console.log('ionViewDidLoad ProfilPage');
-
-
-
-    this.storage.get('user_id').then((val) => {
-
+    this.storage.get('user_id').then((val) =>
+    {
       this.idUser = val;
-      this.userApiProvider.getUser(val).subscribe(data =>{
+      this.userApiProvider.getUser(val).subscribe(data =>
+      {
         this.user = data['data'];
       });
-    }).then( (po) => {
-      this.postApiProvider.getPosts().subscribe(data => {
+    }).then( (po) =>
+    {
+      this.postApiProvider.getPosts().subscribe(data =>
+      {
         this.posts = data['data'];
         console.log(this.posts);
-        this.myPost = this.posts.filter((post) => {
+        this.myPost = this.posts.filter((post) =>
+        {
           return (post.user_id === this.idUser)
         })
-
       })
     })
   }
 
-  goToDetail(post) {
+  //Fonction d'affichage de la page de détail d'un post en transferant les données avec la variable post
+  goToDetail(post)
+  {
     this.navCtrl.push(SpotPage, post);
   }
-
-
-
 
 }
 
